d00f48b6df2a8feabe29baa4064e45bd9790f361
Abhang Pawar <abhangpawar03@gmail.com>
2025-03-18 21:05:18 +0530

    new code

--- BT/btLL.c
+++ BT/btLL.c
@@ -1,36 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Node {
-    char* data ; 
-    struct Node* left ; 
-    struct Node* right ;
-}
+    char data;
+    struct Node* left;
+    struct Node* right;
+};
 
-void main(){
-    struct Node* tree = NULL ; 
-    char* c[] = {"A","B","C","D","E","F","0","G","0","0","0","0","0","0","0"} ;
-    tree = insert(c,0)
-    inorder(tree) ;
-    return 0;
-}
-
-struct Node* insert(char c[] , int i){
-    struct Node* tree = NULL;
-    
-    if(c[n] != NULL){
-        tree = Node* malloc(sizeOf(Node));
-        tree->left = insert(c,2*i+1);
-        tree->data = c[i];
-        tree->right = insert(c,2*i+2);
+// Function to create a new node
+struct Node* createNode(char data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
     }
-    return tree ;
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
 }
 
-void inorder(struct Node* tree){
-    if(tree!=NULL){
+// Function to insert a node into the binary tree
+struct Node* insert(char c[], int n) {
+    if (c[n] == '\0') {
+        return NULL;
+    }
+    
+    struct Node* tree = createNode(c[n]);
+    
+    // Insert left child (2*n+1) and right child (2*n+2)
+    if (2*n+1 < strlen(c) && c[2*n+1] != '\0') {
+        tree->left = insert(c, 2*n+1);
+    }
+    
+    if (2*n+2 < strlen(c) && c[2*n+2] != '\0') {
+        tree->right = insert(c, 2*n+2);
+    }
+    
+    return tree;
+}
+
+// Function to perform inorder traversal
+void inorder(struct Node* tree) {
+    if (tree != NULL) {
         inorder(tree->left);
-        printf("%s",tree->data);
+        printf("%c ", tree->data);
         inorder(tree->right);
     }
+}
+
+// Function to free the memory allocated for the tree
+void freeTree(struct Node* tree) {
+    if (tree != NULL) {
+        freeTree(tree->left);
+        freeTree(tree->right);
+        free(tree);
+    }
+}
+
+int main() {
+    char c[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', '\0'};
+    
+    struct Node* tree = insert(c, 0);
+    
+    printf("Inorder traversal: ");
+    inorder(tree);
+    printf("\n");
+    
+    // Free the allocated memory
+    freeTree(tree);
+    
+    return 0;
 }
